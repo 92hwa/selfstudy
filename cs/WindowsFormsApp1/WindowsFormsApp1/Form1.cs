389fa6710d2420ec3aa55a733c561c9c0403ff16
92hwa <ohyunwha@gmail.com>
2025-09-05 13:19:49 +0900

    ADD

---
+++ cs/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    Mat image = Cv2.ImRead(dlg.FileName);
+
+                    pictureBox1.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(image);
+
+                    label1.Text = $"파일명: {System.IO.Path.GetFileName(dlg.FileName)}\n" +
+                        $"크기: {image.Width} x {image.Height} \n" +
+                        $"채널 수: {image.Channels()} \n" +
+                        $"타입: {image.Type()}";
+                }
+            }
+
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+    }
+}
