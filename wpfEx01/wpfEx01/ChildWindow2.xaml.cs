9400962f861ae77bfeb14b5245d5ab50ce37e0f9
92hwa <ohyunwha@gmail.com>
2025-09-11 15:21:21 +0900

    EDIT: 프로젝트 이름 변경 rawEx01 > wpfEx01

---
+++ wpfEx01/wpfEx01/ChildWindow2.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpfEx01
+{
+    /// <summary>
+    /// ChildWindow2.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ChildWindow2 : Window
+    {
+        public ChildWindow2()
+        {
+            InitializeComponent();
+        }
+        public void SetImage(WriteableBitmap bitmap)
+        {
+            imgBox3.Source = bitmap;
+        }
+    }
+}
