6cdd4ca445938ba6a30c626bb7774b58bfc4e88d
92hwa <ohyunwha@gmail.com>
2025-09-23 16:37:10 +0900

    ADD: MVVM 패턴 적용

---
+++ wpfEx02/wpfEx02/Model/Load.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Globalization;
+
+
+namespace wpfEx02.Model
+{
+    public class Load
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public double WindowC { get; private set; } = 0;
+        public double WindowW { get; private set; } = 0;
+        public byte[] Buffer8 { get; private set; }
+
+        // raw
+        public byte[] LoadRaw(string path)
+        {
+            return File.ReadAllBytes(path);
+        }
+
+        // dicom
+        public void LoadDicom(string path)
+        {
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            BinaryReader reader = new BinaryReader(fs);
+
+            byte[] pixelData = null;
+            reader.BaseStream.Seek(128, SeekOrigin.Begin);
+            string dicm = new string(reader.ReadChars(4));
+
+            while(reader.BaseStream.Position < reader.BaseStream.Length)
+            {
+                ushort group = reader.ReadUInt16();
+                ushort element = reader.ReadUInt16();
+                string vr = Encoding.ASCII.GetString(reader.ReadBytes(2));
+
+                int vl = 0;
+                if (vr == "OB" || vr == "OW" || vr == "SQ" || vr=="UN")
+                {
+                    reader.ReadUInt16();
+                    vl = (int)reader.ReadUInt32();
+                }
+                else
+                {
+                    vl = reader.ReadUInt16();
+                }
+
+                byte[] valueBytes = reader.ReadBytes(vl);
+
+                switch (group)
+                {
+                    case 0x0028:
+                        switch (element)
+                        {
+                            case 0x0010: Height = BitConverter.ToUInt16(valueBytes, 0); break;
+                            case 0x0011: Width = BitConverter.ToUInt16(valueBytes, 0); break;
+                            case 0x1050:
+                                {
+                                    string s = Encoding.ASCII.GetString(valueBytes).Trim('\0', ' ');
+                                    if (!string.IsNullOrEmpty(s))
+                                    {
+                                        var first = s.Split('\\')[0];
+                                        if (double.TryParse(first, NumberStyles.Float, CultureInfo.InvariantCulture, out double wc)) WindowC = wc;
+                                    }
+                                }
+                                break;
+                            case 0x1051:
+                                {
+                                    string s = Encoding.ASCII.GetString(valueBytes).Trim('\0', ' ');
+                                    if(!string.IsNullOrEmpty(s))
+                                    {
+                                        var first = s.Split('\\')[0];
+                                        if (double.TryParse(first, NumberStyles.Float, CultureInfo.InvariantCulture, out double ww)) WindowW = ww;
+                                    }
+                                }
+                                break;
+                        }
+                        break;
+                    case 0x7FE0 when element == 0x0010: pixelData = valueBytes; break;
+                }
+                if (Height > 0 && Width > 0 && pixelData != null) break;
+            }
+            byte[] buffer = new byte[Width * Height];
+
+            for(int i = 0; i < buffer.Length; i++)
+            {
+                if (i * 2 + 1 < pixelData.Length)
+                {
+                    ushort value16 = (ushort)(pixelData[i * 2] | (pixelData[i * 2 + 1] << 8));
+                    buffer[i] = (byte)(value16 >> 8);
+                }
+            }
+        }
+    }
+}
---
+++ wpfEx02/wpfEx02/ViewModel/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using wpfEx02.Model;
+using Microsoft.Win32;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Media.Imaging;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace wpfEx02.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        private readonly Load _loader = new Load();
+
+        [ObservableProperty]
+        private BitmapSource image = null;
+
+        public ICommand LoadCommand { get; }
+
+        public MainViewModel()
+        {
+            LoadCommand = new RelayCommand(OnLoad);
+        }
+
+        private void OnLoad()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "All Files|*.*|RAW Files|*.raw|DICOM Files|*.dcm";
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                string selectedFileExt = Path.GetExtension(selectedFilePath);
+
+                switch (selectedFileExt)
+                {
+                    case "dcm":
+                        {
+                            _loader.LoadDicom(openFileDialog.FileName);
+                            image = BitmapSource.Create(_loader.Width, _loader.Height, 96, 96, pixelFormats.Gray8, null, _loader.Buffer8, _loader.Width);
+                        }
+                        break;
+                    case "raw":
+                        {
+
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
