e20541d14caddf328fd6d6239963af5b01ba89b7
92hwa <ohyunwha@gmail.com>
2025-09-24 16:30:13 +0900

    DELETE: MVVM 시도한 프로젝트

--- wpfEx02/wpfEx02/ViewModel/MainViewModel.cs
+++ wpfEx02/wpfEx02/ViewModel/MainViewModel.cs
@@ -1,20 +1,21 @@
 ﻿using System.IO;
-using wpfEx02.Model;
 using Microsoft.Win32;
 using System.Windows.Input;
-using CommunityToolkit.Mvvm.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
+using wpfEx02.Model;
 
 namespace wpfEx02.ViewModel
 {
     public partial class MainViewModel : ObservableObject
     {
-        private readonly Load _loader = new Load();
+        private readonly FileLoadException _loader = new Load();
 
         [ObservableProperty]
         private BitmapSource image = null;
-
+        
         public ICommand LoadCommand { get; }
 
         public MainViewModel()
@@ -24,29 +25,10 @@ namespace wpfEx02.ViewModel
 
         private void OnLoad()
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "All Files|*.*|RAW Files|*.raw|DICOM Files|*.dcm";
-            openFileDialog.Multiselect = false;
-
-            if (openFileDialog.ShowDialog() == true)
+            var openFileDialog = new OpenFileDialog
             {
-                string selectedFilePath = openFileDialog.FileName;
-                string selectedFileExt = Path.GetExtension(selectedFilePath);
-
-                switch (selectedFileExt)
-                {
-                    case "dcm":
-                        {
-                            _loader.LoadDicom(openFileDialog.FileName);
-                            image = BitmapSource.Create(_loader.Width, _loader.Height, 96, 96, pixelFormats.Gray8, null, _loader.Buffer8, _loader.Width);
-                        }
-                        break;
-                    case "raw":
-                        {
-
-                        }
-                        break;
-                }
+                Filter = "All Files|*.*|RAW Files|*.raw|DICOM Files|*.dcm|Image Files|*.png;*.jpg;",
+                Multiselect = false;
             }
         }
     }
